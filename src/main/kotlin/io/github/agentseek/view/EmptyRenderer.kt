0b915480d2c6bdc329562519fa1eeee2f51848fb
FreshMag <pescomagnani@gmail.com>
2024-10-17 23:21:26 +0200

    refactor: changed EmptyRenderer to its initial state

--- src/main/kotlin/io/github/agentseek/view/EmptyRenderer.kt
+++ src/main/kotlin/io/github/agentseek/view/EmptyRenderer.kt
@@ -1,11 +1,7 @@
 package io.github.agentseek.view
 
 import io.github.agentseek.core.GameObject
-import java.awt.geom.Ellipse2D
-import java.awt.geom.Rectangle2D
 
 class EmptyRenderer(override val layer: Layer = Layer.GENERIC) : Renderer {
-    override fun render(gameObject: GameObject) {
-        GameGui.list.addAll(mutableListOf(Rectangle2D.Float(0f, 0f, 10f, 10f), Ellipse2D.Float(10f, 10f, 10f, 10f)))
-    }
+    override fun render(gameObject: GameObject) {}
 }
