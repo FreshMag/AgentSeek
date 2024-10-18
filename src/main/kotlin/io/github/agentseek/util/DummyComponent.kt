ef98c8e12784a28d6bb1a72fbb88f3d204de5c95
FreshMag <pescomagnani@gmail.com>
2024-10-19 00:46:59 +0200

    feat: added callback for collision inside DummyComponent

--- src/main/kotlin/io/github/agentseek/util/DummyComponent.kt
+++ src/main/kotlin/io/github/agentseek/util/DummyComponent.kt
@@ -5,12 +5,17 @@ import io.github.agentseek.components.AbstractComponent
 import io.github.agentseek.core.GameObject
 import io.github.agentseek.core.engine.GameEngine.log
 import kotlin.time.Duration
-import kotlin.time.DurationUnit
 
 class DummyComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
 
+    override fun init() {
+        gameObject.rigidBody.onCollision {
+            println("I'm colliding!")
+        }
+    }
+
     override fun onUpdate(deltaTime: Duration) {
         log("It's me the Dummy Component! Here's my ID: ${gameObject.id}")
-        gameObject.rigidBody.applyForce(Vector2d(1.0, 1.0))
+        gameObject.rigidBody.applyForce(Vector2d(0.5, 0.25))
     }
 }
