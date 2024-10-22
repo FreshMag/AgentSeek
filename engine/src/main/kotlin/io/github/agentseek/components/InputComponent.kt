afc581c5afeca5e7b6fd1cad0899df23ac423e97
tcapelli <tcapelli@technogym.com>
2024-10-22 22:29:34 +0200

    chore: create InputComponent

---
+++ engine/src/main/kotlin/io/github/agentseek/components/InputComponent.kt
@@ -0,0 +1,19 @@
+package io.github.agentseek.components
+
+import io.github.agentseek.common.Vector2d
+import io.github.agentseek.core.GameObject
+import io.github.agentseek.core.engine.input.Input
+import kotlin.time.Duration
+
+class InputComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
+    override fun onUpdate(deltaTime: Duration) {
+        super.onUpdate(deltaTime)
+        when {
+            Input.UP -> gameObject.rigidBody.applyForce(Vector2d(0.0, -2.0))
+            Input.DOWN -> gameObject.rigidBody.applyForce(Vector2d(0.0, 2.0))
+            Input.RIGHT -> gameObject.rigidBody.applyForce(Vector2d(-2.0, 0.0))
+            Input.LEFT -> gameObject.rigidBody.applyForce(Vector2d(2.0, 0.0))
+            else -> gameObject.rigidBody.velocity = Vector2d(0.0, 0.0)
+        }
+    }
+}
