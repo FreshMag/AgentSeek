553fff76be2c9bcfdedfe4e81c01ab4486957b6c
FreshMag <pescomagnani@gmail.com>
2024-10-23 19:06:24 +0200

    feat: added a simple SightSensorComponent to implement a cone of sight in front of the game object

---
+++ agentseek/src/main/kotlin/io/github/agentseek/components/SightSensorComponent.kt
@@ -0,0 +1,42 @@
+package io.github.agentseek.components
+
+import io.github.agentseek.common.Cone2d
+import io.github.agentseek.core.GameObject
+import io.github.agentseek.physics.RigidBody
+import io.github.agentseek.util.GameObjectUtilities.center
+import io.github.agentseek.view.RenderingContext
+import io.github.agentseek.view.SimpleRenderer
+import io.github.agentseek.view.utilities.Rendering.strokeShape
+import java.awt.Graphics2D
+import kotlin.time.Duration
+
+class SightSensorComponent(gameObject: GameObject, coneLength: Double, coneAperture: Double) : AbstractComponent(gameObject) {
+
+    private val sensorRigidBody: RigidBody = RigidBody.ConeRigidBody(coneAperture, coneLength, 0.0, gameObject)
+    private var lastPos = gameObject.position
+
+    override fun init() {
+        sensorRigidBody.shape.position = gameObject.position
+        val originalRenderer = gameObject.renderer
+        gameObject.renderer = object : SimpleRenderer() {
+            override fun render(gameObject: GameObject, renderingContext: RenderingContext<Graphics2D>?) {
+                renderingContext?.strokeShape(sensorRigidBody.shape)
+                originalRenderer.applyOnView(gameObject)
+            }
+        }
+    }
+
+    override fun onUpdate(deltaTime: Duration) {
+        sensorRigidBody.shape.position = gameObject.center()
+        if (lastPos != gameObject.position) {
+            val direction = gameObject.position - lastPos
+            val angle = direction.angle()
+            (sensorRigidBody.shape as? Cone2d)?.rotation = angle
+            lastPos = gameObject.position
+        }
+        if (sensorRigidBody.findColliding().isNotEmpty()) {
+            println("I'm seeing something!")
+        }
+    }
+
+}
--- agentseek/src/main/kotlin/io/github/agentseek/view/SimpleRenderer.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/view/SimpleRenderer.kt
@@ -4,7 +4,7 @@ import io.github.agentseek.core.GameObject
 import io.github.agentseek.view.utilities.Rendering.strokeShape
 import java.awt.Graphics2D
 
-class SimpleRenderer(override val layer: Layer = Layer.GENERIC) : Renderer<Graphics2D> {
+open class SimpleRenderer(override val layer: Layer = Layer.GENERIC) : Renderer<Graphics2D> {
     override fun render(gameObject: GameObject, renderingContext: RenderingContext<Graphics2D>?) {
         renderingContext?.strokeShape(gameObject.rigidBody.shape) ?: return
     }
