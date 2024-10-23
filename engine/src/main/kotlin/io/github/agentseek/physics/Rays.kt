41c17eb0fcb3ab9286971e7569c8f8356404617f
FreshMag <pescomagnani@gmail.com>
2024-10-23 15:53:20 +0200

    fix: fixed bugged intersection calculation between rays and rectangles and cones

--- engine/src/main/kotlin/io/github/agentseek/physics/Rays.kt
+++ engine/src/main/kotlin/io/github/agentseek/physics/Rays.kt
@@ -2,15 +2,20 @@ package io.github.agentseek.physics
 
 import io.github.agentseek.common.*
 import io.github.agentseek.core.GameObject
+import io.github.agentseek.util.GameObjectUtilities.center
 import io.github.agentseek.util.GameObjectUtilities.otherGameObjects
-import kotlin.math.cos
-import kotlin.math.max
-import kotlin.math.min
+import kotlin.math.*
 
 object Rays {
 
     class Ray(val gameObject: GameObject, vector2d: Vector2d) {
         val direction = vector2d.normalized()
+        private var other: GameObject? = null
+
+        constructor(origin: GameObject, destination: GameObject)
+                : this(origin, destination.center() - origin.center()) {
+            other = destination
+        }
 
         /**
          * Returns the first GameObject intersecting with this Ray
@@ -44,7 +49,7 @@ object Rays {
      * Casts a ray from this GameObject to another (using the vector between their position)
      */
     fun GameObject.castRay(gameObject: GameObject): Ray =
-        Ray(this, this.position - gameObject.position)
+        Ray(this, gameObject)
 
     /**
      * Checks if this Shape intersects with a [Ray]
@@ -58,7 +63,7 @@ object Rays {
 
 
     private fun Circle2d.circleRayIntersect(ray: Ray): Boolean {
-        val oc = ray.gameObject.position - center
+        val oc = ray.gameObject.center() - center
         val direction = ray.direction
 
         // Coefficients for the quadratic equation
@@ -67,29 +72,30 @@ object Rays {
         val c = oc.dot(oc) - radius * radius
 
         val discriminant = b * b - 4 * a * c
-        return discriminant >= 0
+        // Since this method treats the ray as an infinite straight line, we need to exclude "backwards" shapes
+        return discriminant >= 0 && abs(ray.direction.angleWith(oc)) >= Math.PI / 2
     }
 
     private fun Rectangle2d.rectangleRayIntersect(ray: Ray): Boolean {
-        val invDir = Vector2d(1.0 / ray.direction.x, 1.0 / ray.direction.y)
+        val rayOrigin = ray.gameObject.center()
+        val direction = ray.direction
 
-        val tMin = (upperLeft - ray.gameObject.position).componentWiseMul(invDir)
-        val tMax = (lowerRight - ray.gameObject.position).componentWiseMul(invDir)
+        val minX = upperLeft.x
+        val maxX = lowerRight.x
+        val minY = upperLeft.y
+        val maxY = lowerRight.y
+        val t1 = (minX - rayOrigin.x) / direction.x
+        val t2 = (maxX - rayOrigin.x) / direction.x
+        val t3 = (minY - rayOrigin.y) / direction.y
+        val t4 = (maxY - rayOrigin.y) / direction.y
+        val tMin = maxOf(min(t1, t2), min(t3, t4))
+        val tMax = minOf(max(t1, t2), max(t3, t4))
 
-        val t1 = min(tMin.x, tMax.x)
-        val t2 = max(tMin.x, tMax.x)
-        val t3 = min(tMin.y, tMax.y)
-        val t4 = max(tMin.y, tMax.y)
-
-        val tNear = max(t1, t3)
-        val tFar = min(t2, t4)
-
-        // If tNear > tFar, no intersection
-        return tNear <= tFar && tFar >= 0.0
+        return tMin <= tMax && tMax >= 0.0
     }
 
     private fun Cone2d.coneRayIntersect(ray: Ray): Boolean {
-        val oc = ray.gameObject.position - vertex
+        val oc = ray.gameObject.center() - vertex
         val cosThetaSq = cos(angle) * cos(angle)
 
         val direction = ray.direction
@@ -100,7 +106,7 @@ object Rays {
         val c = oc.x * oc.x + oc.y * oc.y - cosThetaSq * oc.dot(oc)
 
         val discriminant = b * b - 4 * a * c
-
-        return discriminant >= 0
+        // Since this method treats the ray as an infinite straight line, we need to exclude "backwards" shapes
+        return discriminant >= 0 && abs(ray.direction.angleWith(oc)) >= Math.PI / 2
     }
 }
