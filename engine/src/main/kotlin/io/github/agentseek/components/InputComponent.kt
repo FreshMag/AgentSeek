7551e11e879372c8878e5a7beec356d21362f0f1
tcapelli <tcapelli@technogym.com>
2024-10-26 17:27:10 +0200

    refactor: use companion object for default value

--- engine/src/main/kotlin/io/github/agentseek/components/InputComponent.kt
+++ engine/src/main/kotlin/io/github/agentseek/components/InputComponent.kt
@@ -6,10 +6,14 @@ import io.github.agentseek.core.engine.input.Input
 import kotlin.time.Duration
 
 class InputComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
+    companion object {
+        const val STANDARD_VELOCITY = 2.0
+    }
+
     override fun onUpdate(deltaTime: Duration) {
         gameObject.rigidBody.velocity = Vector2d(
-            if (Input.RIGHT) 2.0 else if (Input.LEFT) -2.0 else 0.0,
-            if (Input.UP) -2.0 else if (Input.DOWN) 2.0 else 0.0
+            if (Input.RIGHT) STANDARD_VELOCITY else if (Input.LEFT) -STANDARD_VELOCITY else 0.0,
+            if (Input.UP) -STANDARD_VELOCITY else if (Input.DOWN) STANDARD_VELOCITY else 0.0
         )
     }
 }
