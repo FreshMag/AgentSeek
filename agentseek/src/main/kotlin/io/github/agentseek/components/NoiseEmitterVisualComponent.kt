4bb6d4326497cccc177219b1f9c024dfff9f8daa
tcapelli <tcapelli@technogym.com>
2024-10-29 20:35:49 +0100

    refactor: use VFX with given interval

--- agentseek/src/main/kotlin/io/github/agentseek/components/NoiseEmitterVisualComponent.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/components/NoiseEmitterVisualComponent.kt
@@ -8,12 +8,21 @@ import kotlin.time.Duration
 
 @Requires(NoiseEmitterComponent::class)
 class NoiseEmitterVisualComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
-
+    private var lastNoiseEmittedTimeMillis: Long? = null
     override fun onUpdate(deltaTime: Duration) {
         gameObject.getComponent<NoiseEmitterComponent>()!!.getNoiseEmitterCollider()?.let {
-            VFX.expandingCircle(
-                worldPosition = it.center, color = Color.gray, speed = 3, maxRadius = (it.shape as Circle2d).radius + 1
-            )
+            if (hasTimeElapsed()) {
+                lastNoiseEmittedTimeMillis = System.currentTimeMillis()
+                VFX.expandingCircle(
+                    worldPosition = it.center, color = Color.black, speed = 1, maxRadius = (it.shape as Circle2d).radius
+                )
+            }
         }
+
+    }
+
+    private fun hasTimeElapsed(): Boolean {
+        val currentTime = System.currentTimeMillis()
+        return lastNoiseEmittedTimeMillis == null || currentTime - lastNoiseEmittedTimeMillis!! >= 300
     }
 }
