05c0f765ea6388396028ac7044e212f1111aded3
tcapelli <tcapelli@technogym.com>
2024-10-29 20:35:30 +0100

    feat: implement NoiseLevel logic

--- agentseek/src/main/kotlin/io/github/agentseek/components/NoiseEmitterComponent.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/components/NoiseEmitterComponent.kt
@@ -1,32 +1,33 @@
 package io.github.agentseek.components
 
-import io.github.agentseek.common.Circle2d
 import io.github.agentseek.core.GameObject
 import io.github.agentseek.physics.Collider
-import io.github.agentseek.util.GameObjectUtilities.attachRenderer
 import io.github.agentseek.util.GameObjectUtilities.center
-import io.github.agentseek.view.utilities.Rendering.fillGradientCircle
-import io.github.agentseek.view.utilities.Rendering.strokeCircle
-import java.awt.Color
 import kotlin.time.Duration
 
-class NoiseEmitterComponent(gameObject: GameObject, radius: Double) : AbstractComponent(gameObject) {
-    private val noiseEmitterCollider: Collider = Collider.CircleCollider(radius, gameObject)
+private enum class NoiseLevel {
+    LOW, DEFAULT, HIGH
+}
+
+class NoiseEmitterComponent(gameObject: GameObject, private val radius: Double) : AbstractComponent(gameObject) {
+    companion object {
+        const val STANDARD_VELOCITY = 2.0
+    }
+
     private var isEmittingNoise = false
-    private var lastPos = gameObject.position
+    private var lastPos = gameObject.center()
+    private var noiseEmitterCollider: Collider = Collider.CircleCollider(radius, gameObject)
     override fun init() {
         noiseEmitterCollider.center = gameObject.center()
-        gameObject.attachRenderer { _, renderingContext ->
-            renderingContext?.fillGradientCircle(
-                noiseEmitterCollider.shape as Circle2d, Color(245, 154, 165), Color(255, 255, 0, 0)
-            )
-            renderingContext?.strokeCircle(
-                noiseEmitterCollider.shape as Circle2d, color = Color.BLACK
-            )
-        }
     }
 
     override fun onUpdate(deltaTime: Duration) {
+        val noiseRadius = when (getNoiseLevel()) {
+            NoiseLevel.LOW -> radius / 2
+            NoiseLevel.DEFAULT -> radius
+            NoiseLevel.HIGH -> radius * 2
+        }
+        noiseEmitterCollider = Collider.CircleCollider(noiseRadius, gameObject)
         noiseEmitterCollider.center = gameObject.center()
         if (lastPos != gameObject.center()) {
             lastPos = gameObject.center()
@@ -34,10 +35,19 @@ class NoiseEmitterComponent(gameObject: GameObject, radius: Double) : AbstractCo
         } else {
             isEmittingNoise = false
         }
+
     }
 
     fun getNoiseEmitterCollider(): Collider? {
         return if (isEmittingNoise) noiseEmitterCollider
         else null
+    }
+
+    private fun getNoiseLevel(): NoiseLevel {
+        return if (gameObject.rigidBody.velocity.x >= STANDARD_VELOCITY || gameObject.rigidBody.velocity.y >= STANDARD_VELOCITY) {
+            NoiseLevel.DEFAULT
+        } else {
+            NoiseLevel.LOW
+        }
     }
 }
