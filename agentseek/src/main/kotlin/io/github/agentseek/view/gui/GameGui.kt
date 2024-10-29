0e0df7600c411dfd811cbe2757f14ab3451a1092
FreshMag <pescomagnani@gmail.com>
2024-10-29 20:32:19 +0100

    fix: fixed different used scene after merge

--- agentseek/src/main/kotlin/io/github/agentseek/view/gui/GameGui.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/view/gui/GameGui.kt
@@ -43,7 +43,7 @@ object GameGui : View, InputListener() {
         }
     }
 
-    fun startGameGui(useRepl: Boolean = false, scene: Scene = Scenes.exampleScene()) {
+    fun startGameGui(useRepl: Boolean = false, scene: Scene = Scenes.mapBordersScene()) {
         frame.name = APP_NAME
         val panel = GameViewPanel(screenSize, gameViewRendering)
         frame.add(panel, BorderLayout.CENTER)
