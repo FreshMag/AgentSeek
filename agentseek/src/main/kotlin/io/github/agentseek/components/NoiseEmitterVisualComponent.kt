d45273e4474d9a457d0d17690b79232d8d455720
tcapelli <tcapelli@technogym.com>
2024-10-29 21:15:52 +0100

    refactor: use Timer

--- agentseek/src/main/kotlin/io/github/agentseek/components/NoiseEmitterVisualComponent.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/components/NoiseEmitterVisualComponent.kt
@@ -1,6 +1,7 @@
 package io.github.agentseek.components
 
 import io.github.agentseek.common.Circle2d
+import io.github.agentseek.common.TimerImpl
 import io.github.agentseek.core.GameObject
 import io.github.agentseek.view.animations.VFX
 import java.awt.Color
@@ -8,21 +9,20 @@ import kotlin.time.Duration
 
 @Requires(NoiseEmitterComponent::class)
 class NoiseEmitterVisualComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
-    private var lastNoiseEmittedTimeMillis: Long? = null
+    private val timer = TimerImpl(500)
+    override fun init() {
+        timer.startTimer()
+    }
+
     override fun onUpdate(deltaTime: Duration) {
         gameObject.getComponent<NoiseEmitterComponent>()!!.getNoiseEmitterCollider()?.let {
-            if (hasTimeElapsed()) {
-                lastNoiseEmittedTimeMillis = System.currentTimeMillis()
+            if (timer.isElapsed()) {
+                timer.restart()
                 VFX.expandingCircle(
                     worldPosition = it.center, color = Color.black, speed = 1, maxRadius = (it.shape as Circle2d).radius
                 )
             }
         }
 
-    }
-
-    private fun hasTimeElapsed(): Boolean {
-        val currentTime = System.currentTimeMillis()
-        return lastNoiseEmittedTimeMillis == null || currentTime - lastNoiseEmittedTimeMillis!! >= 300
     }
 }
--- agentseek/src/main/kotlin/io/github/agentseek/components/NoiseSensorVisualComponent.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/components/NoiseSensorVisualComponent.kt
@@ -1,5 +1,6 @@
 package io.github.agentseek.components
 
+import io.github.agentseek.common.TimerImpl
 import io.github.agentseek.core.GameObject
 import io.github.agentseek.view.animations.VFX
 import java.awt.Color
@@ -8,28 +9,19 @@ import kotlin.time.Duration
 @Requires(NoiseSensorComponent::class)
 class NoiseSensorVisualComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
     private lateinit var noiseSensor: NoiseSensorComponent
-    private var lastNoiseHeardTimeMillis: Long? = null
-
+    private val timer = TimerImpl(DEFAULT_SUSPICIOUS_TIME_MILLIS.toLong())
     override fun init() {
         noiseSensor = gameObject.getComponent<NoiseSensorComponent>()!!
+        timer.startTimer()
     }
 
     override fun onUpdate(deltaTime: Duration) {
-        if (noiseSensor.getNoiseFound() && hasTimeElapsed()) {
-            lastNoiseHeardTimeMillis = System.currentTimeMillis()
+        if (noiseSensor.getNoiseFound() && timer.isElapsed()) {
+            timer.restart()
             VFX.fadingText(
-                worldPosition = gameObject.position,
-                text = "?",
-                color = Color.BLACK,
-                size = 40,
-                durationMillis = 3000
+                worldPosition = gameObject.position, text = "?", color = Color.BLACK, size = 40, durationMillis = 3000
             )
         }
-    }
-
-    private fun hasTimeElapsed(): Boolean {
-        val currentTime = System.currentTimeMillis()
-        return lastNoiseHeardTimeMillis == null || currentTime - lastNoiseHeardTimeMillis!! >= DEFAULT_SUSPICIOUS_TIME_MILLIS
     }
 
     private companion object {
