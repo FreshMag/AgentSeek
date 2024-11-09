c158dfb6b8d74d768e452c91b6a0d01fcadf1944
tcapelli <tcapelli@technogym.com>
2024-11-09 17:12:47 +0100

    feat: define new actions

--- agentseek/src/main/kotlin/io/github/agentseek/env/Actions.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/env/Actions.kt
@@ -8,4 +8,14 @@ object Actions {
      * Linking action for a Jason agent. It links with the engine action.
      */
     val linkAction: Literal = Literal.parseLiteral("link(this)")
+
+    /**
+     * Action to simulate a random walk.
+     */
+    val moveRandom: Literal = Literal.parseLiteral("moveRandom")
+
+    /**
+     * Action to make the agent follow an enemy.
+     */
+    val moveToPosition: Literal = Literal.parseLiteral("move")
 }
