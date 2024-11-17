fa82f07f837b49591791be9da7b21402dc72c168
FreshMag <pescomagnani@gmail.com>
2024-11-17 10:38:07 +0100

    feat: added option to set direction, stop and restart inside FieldMovementComponent.kt

--- agentseek/src/main/kotlin/io/github/agentseek/components/FieldMovementComponent.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/components/FieldMovementComponent.kt
@@ -10,16 +10,23 @@ import kotlin.time.Duration
 class FieldMovementComponent(gameObject: GameObject) : AbstractComponent(gameObject) {
     private lateinit var sensor: DistanceSensorComponent
     private var previousDirection = Vector2d.zero()
+    private var directionObjective: Vector2d = Vector2d.zero()
     private val forwardDirection
-        get() = objective - gameObject.center()
+        get() = objective?.let { it - gameObject.center() } ?: directionObjective
 
-    var objective: Point2d = Point2d(50.0, 50.0)
+    var objective: Point2d? = Point2d(50.0, 50.0)
+
+    private var isStopped: Boolean = false
 
     override fun init() {
         sensor = gameObject.getComponent<DistanceSensorComponent>()!!
     }
 
     override fun onUpdate(deltaTime: Duration) {
+        if (isStopped) {
+            gameObject.rigidBody.velocity = Vector2d.zero()
+            return
+        }
         val distances = sensor.getDistancesResultant()
         val direction: Vector2d = previousDirection +
                 if (distances != Vector2d.zero()) {
@@ -39,9 +46,22 @@ class FieldMovementComponent(gameObject: GameObject) : AbstractComponent(gameObj
         gameObject.rigidBody.velocity = previousDirection * MAX_VELOCITY
     }
 
+    fun setDirection(direction: Vector2d) {
+        objective = null
+        directionObjective = direction.normalized()
+    }
+
+    fun stop() {
+        isStopped = true
+    }
+
+    fun wakeUp() {
+        isStopped = false
+    }
+
     companion object {
         const val TANGENTIAL_DEGREES = 115.0
-        const val MAX_VELOCITY = 1.0
+        const val MAX_VELOCITY = 2.0
         const val DANGER_COEFFICIENT = 0.3
     }
 }
