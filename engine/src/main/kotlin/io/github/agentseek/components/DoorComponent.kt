88743b81346f61b7749f1960a64f5e75d62135d3
FreshMag <pescomagnani@gmail.com>
2024-11-18 10:43:40 +0100

    feat: added basic door behavior

--- engine/src/main/kotlin/io/github/agentseek/components/DoorComponent.kt
+++ engine/src/main/kotlin/io/github/agentseek/components/DoorComponent.kt
@@ -1,31 +1,23 @@
 package io.github.agentseek.components
 
-import io.github.agentseek.common.Timer
-import io.github.agentseek.common.TimerImpl
 import io.github.agentseek.core.GameObject
 import io.github.agentseek.events.NewLevelEvent
-import kotlin.time.Duration
 
 /**
  * Component used to describe door's behavior.
  */
-class DoorComponent(gameObject: GameObject, duration: Int) : AbstractComponent(gameObject) {
-    /**
-     * Checks if the door is collidable.
-     */
-    private var isCollidable: Boolean = false
-    private val timer: Timer = TimerImpl(duration.toLong())
+class DoorComponent(gameObject: GameObject, val destinationSceneResourceName: String) : AbstractComponent(gameObject) {
+    private val rigidBody = gameObject.rigidBody
 
     override fun init() {
-        timer.startTimer()
+        rigidBody.onCollision { gameObject ->
+            if (gameObject.name == PLAYER_GO_NAME) {
+                notifyEvent(NewLevelEvent(destinationSceneResourceName))
+            }
+        }
     }
 
-    override fun onUpdate(deltaTime: Duration) {
-        if (timer.isElapsed() || isCollidable) {
-            this.isCollidable = true
-//            if (player?.rigidBody?.isCollidingWith(gameObject.rigidBody) == true) {
-//                notifyEvent(NewLevelEvent())
-//            }
-        }
+    companion object {
+        const val PLAYER_GO_NAME = "Player"
     }
 }
--- engine/src/main/kotlin/io/github/agentseek/events/NewLevelEvent.kt
+++ engine/src/main/kotlin/io/github/agentseek/events/NewLevelEvent.kt
@@ -1,12 +1,19 @@
 package io.github.agentseek.events
 
 import io.github.agentseek.core.Scene
+import io.github.agentseek.core.engine.GameEngine
+import io.github.agentseek.core.engine.GameEngine.log
+import io.github.agentseek.util.serialization.Scenes.loadSceneFromResource
 
 /**
  * This class manages a NewLevel Event and implements Event interface.
  */
-class NewLevelEvent : Event {
-    override fun handle(state: Scene) {
+class NewLevelEvent(val destinationSceneResourceName: String) : Event {
 
+    override fun handle(state: Scene) {
+        val newScene = loadSceneFromResource(destinationSceneResourceName)
+        newScene?.let {
+            GameEngine.loadScene(it)
+        } ?: log("A NewLevelEvent was notified, but $destinationSceneResourceName was not found among resources.")
     }
 }
