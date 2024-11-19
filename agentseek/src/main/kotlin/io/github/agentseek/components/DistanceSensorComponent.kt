ff0fcba93124c6004c63d7cef88975f73bb9a190
FreshMag <pescomagnani@gmail.com>
2024-11-19 17:41:34 +0100

    refactor: deeply improved DistanceSensorComponent.kt calculation of proximity vectors

--- agentseek/src/main/kotlin/io/github/agentseek/components/DistanceSensorComponent.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/components/DistanceSensorComponent.kt
@@ -2,46 +2,48 @@ package io.github.agentseek.components
 
 import io.github.agentseek.common.Vector2d
 import io.github.agentseek.core.GameObject
-import io.github.agentseek.physics.Collider
 import io.github.agentseek.physics.Rays.castRay
+import io.github.agentseek.util.FastEntities.allDirections8
+import io.github.agentseek.util.GameObjectUtilities.attachRenderer
 import io.github.agentseek.util.GameObjectUtilities.center
-import kotlin.time.Duration
+import io.github.agentseek.view.utilities.Rendering.drawVector
+import java.awt.Color
 
 class DistanceSensorComponent(
     gameObject: GameObject, private val radius: Double
 ) : AbstractComponent(gameObject) {
-    private val sensorCollider: Collider = Collider.CircleCollider(radius, gameObject)
+
+    var debugVectors: List<Vector2d> = emptyList()
 
     override fun init() {
-        sensorCollider.center = gameObject.center()
+        gameObject.attachRenderer { _, context ->
+            debugVectors.forEach { v -> context?.drawVector(gameObject.center(), v, Color.RED) }
+        }
     }
 
     /**
      * Gets the resultant obtained adding all vectors of distances from nearby objects within [radius]
      */
-    fun getDistancesResultant(): Vector2d {
-        val colliding = sensorCollider.findColliding().filterNot { it.gameObject.name == "Player" }
-        return if (colliding.isNotEmpty()) {
-            val checked = mutableSetOf<String>()
-            val danger =
-                colliding.fold(Vector2d.zero()) { resultant, collider ->
-                    val go = collider.gameObject
-                    val ray = gameObject.castRay(go)
-                    val intersection = ray.firstIntersecting ?: return@fold resultant
-                    if (intersection.gameObject.id !in checked) {
-                        checked.add(intersection.gameObject.id)
-                        resultant + (ray.direction * intersection.distance)
+    fun getDistancesResultant(): Vector2d = calculateDanger()
+
+    private fun Iterable<Vector2d>.sumUp(): Vector2d = fold(Vector2d.zero()) { acc, vector -> acc + vector }
+
+    private fun calculateDanger(): Vector2d {
+        return (allDirections8())
+            .mapNotNull { direction ->
+                val first = gameObject.castRay(direction).firstIntersecting
+                first?.let {
+                    if (it.distance < radius) {
+                        direction * (radius - it.distance)
                     } else {
-                        resultant
+                        null
                     }
                 }
-            danger.normalized() * (radius - danger.module())
-        } else {
-            Vector2d.zero()
-        }
+            }
+            .also {
+                debugVectors = it
+            }
+            .sumUp()
     }
 
-    override fun onUpdate(deltaTime: Duration) {
-        sensorCollider.center = gameObject.center()
-    }
 }
