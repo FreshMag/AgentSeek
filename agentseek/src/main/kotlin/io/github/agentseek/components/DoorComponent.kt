cae5424e6e0356c2f28fdebacc819c0424c95a76
FreshMag <pescomagnani@gmail.com>
2024-11-25 17:01:12 +0100

    refactor: moved door component to agentseek project

--- agentseek/src/main/kotlin/io/github/agentseek/components/DoorComponent.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/components/DoorComponent.kt
@@ -1,23 +1,26 @@
 package io.github.agentseek.components
 
+import io.github.agentseek.components.common.Config
 import io.github.agentseek.core.GameObject
 import io.github.agentseek.events.NewLevelEvent
 
 /**
  * Component used to describe door's behavior.
  */
-class DoorComponent(gameObject: GameObject, val destinationSceneResourceName: String) : AbstractComponent(gameObject) {
+class DoorComponent(
+    gameObject: GameObject,
+    /**
+     * Name of the scene resource to load when the player collides with the door.
+     */
+    val destinationSceneResourceName: String
+) : AbstractComponent(gameObject) {
     private val rigidBody = gameObject.rigidBody
 
     override fun init() {
         rigidBody.onCollision { gameObject ->
-            if (gameObject.name == PLAYER_GO_NAME) {
+            if (gameObject.name == Config.Player.name) {
                 notifyEvent(NewLevelEvent(destinationSceneResourceName))
             }
         }
-    }
-
-    companion object {
-        const val PLAYER_GO_NAME = "Player"
     }
 }
