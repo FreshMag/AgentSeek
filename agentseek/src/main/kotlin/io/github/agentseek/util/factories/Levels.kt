4c5ff5826774a6534811309d31b74b5a630d3af0
tcapelli <tcapelli@technogym.com>
2024-11-25 21:33:15 +0100

    feat: implement raw path level

--- agentseek/src/main/kotlin/io/github/agentseek/util/factories/Levels.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/util/factories/Levels.kt
@@ -5,6 +5,7 @@ import io.github.agentseek.core.Scene
 import io.github.agentseek.env.AgentSeekEnvironment
 import io.github.agentseek.util.FastEntities.bounds
 import io.github.agentseek.util.FastEntities.gameObject
+import io.github.agentseek.util.FastEntities.point
 import io.github.agentseek.util.FastEntities.scene
 import io.github.agentseek.util.factories.GameObjects.cameraAgent
 import io.github.agentseek.util.factories.GameObjects.door
@@ -23,7 +24,7 @@ object Levels {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        GameGui.startGameGui(scene = prisonLevel())
+        GameGui.startGameGui(scene = pathLevel())
     }
 
     fun prisonLevel(): Scene = useGrid(rows = 5, columns = 7, 6.5) { grid ->
@@ -196,6 +197,165 @@ object Levels {
             )
         )
     }
+
+    fun randomLevel(): Scene = useGrid(rows = 5, columns = 7, 6.5) { grid ->
+        sceneWithJason(
+            name = "easy", environmentClass = AgentSeekEnvironment::class,
+            agents(
+                cameraAgent(
+                    id = "camera1",
+                    position = grid(0, 2.5)
+                ),
+                guardAgent(
+                    id = "agent1",
+                    position = grid(2.5, 2.5),
+                )
+            ),
+            player(
+                position = grid(0.65, 5.35)
+            ),
+            *bounds(
+                grid.boundsSize,
+                GameGui.defaultRenderer(),
+                GameGui.camera.viewPortWidth,
+                GameGui.camera.viewPortHeight
+            ),
+            *walls(
+                wall(
+                    position = grid(2, 2),
+                    width = grid.cellWidth * 1.5,
+                    height = grid.cellHeight / 2,
+                    isCenter = false
+                ),
+                wall(
+                    position = grid(2, 5),
+                    width = grid.cellWidth / 2,
+                    height = grid.cellHeight * 2,
+                    isCenter = false
+                ),
+                wall(
+                    position = grid(1.5, 1.75),
+                    width = grid.cellWidth / 2,
+                    height = grid.cellHeight * 2,
+                    isCenter = true
+                ),
+                wall(
+                    position = grid(3, 0.5),
+                    width = grid.cellWidth / 2,
+                    height = grid.cellHeight * 2,
+                ),
+                wall(
+                    position = grid(0, 3),
+                    width = grid.cellWidth / 2,
+                    height = grid.cellHeight * 3
+                ),
+                wall(
+                    position = grid(1, 5),
+                    width = grid.cellWidth * 2,
+                    height = grid.cellHeight / 2,
+                    isCenter = false
+                ),
+            ),
+            door(
+                destinationSceneName = "prisonScene",
+                position = grid(-1.30, 0.5),
+                size = grid.boundsSize
+            )
+        )
+    }
+
+    fun pathLevel(): Scene = sceneWithJason(
+        name = "example", environmentClass = AgentSeekEnvironment::class,
+        agents = agents(
+            cameraAgent(
+                id = "camera1",
+                position = point(GameGui.camera.viewPortWidth - 4, GameGui.camera.viewPortHeight - 10),
+            ), guardAgent(
+                id = "agent1",
+                position = point(8, 4),
+            ),
+            guardAgent(
+                id = "agent2",
+                position = point(35, 5),
+            )
+        ),
+        player(
+            position = point(GameGui.camera.viewPortWidth - 5, GameGui.camera.viewPortHeight - 5.5)
+        ),
+        *bounds(2.5, GameGui.defaultRenderer(), GameGui.camera.viewPortWidth, GameGui.camera.viewPortHeight),
+        *walls(
+            wall(
+                x = GameGui.camera.viewPortWidth - 10,
+                y = GameGui.camera.viewPortHeight - 8,
+                width = 15,
+                height = 1,
+            ),
+            wall(
+                x = 35,
+                y = 7,
+                width = 10,
+                height = 1,
+            ),
+            wall(
+                x = 20,
+                y = 13,
+                width = 20,
+                height = 1,
+            ),
+            wall(
+                x = 10,
+                y = 23,
+                width = 35,
+                height = 1,
+            ),
+            wall(
+                x = 10,
+                y = 10,
+                width = 1,
+                height = 16,
+            ),
+            wall(
+                x = 15,
+                y = 20,
+                width = 1,
+                height = 5,
+            ),
+            wall(
+                x = 23,
+                y = 20,
+                width = 1,
+                height = 5,
+            ),
+            wall(
+                x = 35,
+                y = 18,
+                width = 1,
+                height = 10,
+            ),
+            wall(
+                x = 40,
+                y = 19,
+                width = 1,
+                height = 5,
+            ),
+            wall(
+                x = 31,
+                y = 19,
+                width = 7,
+                height = 1,
+            ),
+            wall(
+                x = 28,
+                y = 26,
+                width = 1,
+                height = 14,
+            )
+        ),
+        door(
+            "jasonExample",
+            position = point(0, 10)
+        )
+    )
 
     fun gameOverScene(): Scene =
         scene(
