c3c7e3584cfd82ad753104e1a3118386d8f4d686
FreshMag <pescomagnani@gmail.com>
2024-11-26 18:50:26 +0100

    feat: added level saving and changed main level in the gui

--- agentseek/src/main/kotlin/io/github/agentseek/view/gui/GameGui.kt
+++ agentseek/src/main/kotlin/io/github/agentseek/view/gui/GameGui.kt
@@ -99,7 +99,7 @@ object GameGui : View, InputListener() {
      * @param useRepl Whether to use the REPL for the game.
      * @param scene The initial scene to load.
      */
-    fun startGameGui(useRepl: Boolean = false, scene: Scene = Levels.randomLevel()) {
+    fun startGameGui(useRepl: Boolean = false, scene: Scene = Levels.startingLevel()) {
         frame.name = Config.GUI.frameTitle
         val panel = gameView
         frame.add(panel, BorderLayout.CENTER)
